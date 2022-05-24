7c887310d180f866e379906178b50c978bb3e8b7
Alex <a>
2022-05-24 14:16:16 +0300

    refactor #2

--- resources/js/react_frontend/components/LoginForm/index.tsx
+++ resources/js/react_frontend/components/LoginForm/index.tsx
@@ -21,7 +21,10 @@ function LoginForm(props: LoginFormProps) {
 
     const {t, i18n} = useTranslation('common');
     const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [confPassword, setConfPassword] = useState('');
     const [password, setPassword] = useState('');
+    const [register, setRegister] = useState(false);
     const loginRequest = async () => {
         let req = await fetch('/api/auth/login', {
             method: 'POST',
@@ -40,32 +43,106 @@ function LoginForm(props: LoginFormProps) {
             return {authorized: false, token: null, userID: null, role: "guest"};
         }
     }
+
+    const registerRequest = async () => {
+        let req = await fetch('/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+
+            },
+            body: JSON.stringify({"email": email, "password": password, "name": name})
+        });
+        if (req.ok) {
+            let response = await req.json();
+
+            return {success: true};
+        } else {
+            return {success: false};
+        }
+    }
     return (
         <div>
-            <TextField label="email" value={email} onChange={(e, newValue) => {
-                if (typeof newValue == 'string') {
-                    setEmail(newValue);
-                }
-            }}/>
+            <LinkF onClick={() => {
+                setRegister(!register)
+                setConfPassword('');
+                setName('');
+                setPassword('');
+                setEmail('');
+            }}>{register ? "Sign UP" : "Login"}</LinkF>
+            {register ?
+                <>
+                    <TextField label="name" value={name} onChange={(e, newValue) => {
+                        if (typeof newValue == 'string') {
+                            setName(newValue);
+                        }
+                    }}/>
+                    <TextField label="email" value={email} onChange={(e, newValue) => {
+                        if (typeof newValue == 'string') {
+                            setEmail(newValue);
+                        }
+                    }}/>
 
-            <TextField label="password"
-                       type={'password'}
-                       value={password} onChange={(e, newValue) => {
-                if (typeof newValue == 'string') {
-                    setPassword(newValue);
-                }
-            }}/>
+                    <TextField label="password"
+                               type={'password'}
+                               value={password} onChange={(e, newValue) => {
+                        if (typeof newValue == 'string') {
+                            setPassword(newValue);
+                        }
+                    }}/>
+                    <TextField label="confirm password"
+                               type={'password'}
+                               value={confPassword} onChange={(e, newValue) => {
+                        if (typeof newValue == 'string') {
+                            setConfPassword(newValue);
+                        }
+                    }}/>
 
-            <PrimaryButton text="LOGIN" onClick={() => {
-                loginRequest().then(value => {
-                    if (value.authorized) {
-                        props.dispatch(AuthLoginAction(value));
-                    }
-                })
+                    <PrimaryButton text="SIGN UP" allowDisabledFocus
+                                   disabled={email.length == 0 || name.length < 2 || password.length < 8 || confPassword.length < 8 ||password!==confPassword}
+                                   onClick={() => registerRequest().then(v => {
+                                       if (v.success) {
+                                           setConfPassword('');
+                                           setName('');
+                                           setPassword('');
+                                           setEmail('');
+                                           alert("Success register!");
+                                       } else {
+                                           alert("Not registered!!!!");
+                                       }
+                                   })}
+                    />
+                </>
+                :
+                <>
+                    <TextField label="email" value={email} onChange={(e, newValue) => {
+                        if (typeof newValue == 'string') {
+                            setEmail(newValue);
+                        }
+                    }}/>
 
-            }} allowDisabledFocus
-            />
+                    <TextField label="password"
+                               type={'password'}
+                               value={password} onChange={(e, newValue) => {
+                        if (typeof newValue == 'string') {
+                            setPassword(newValue);
+                        }
+                    }}/>
 
+                    <PrimaryButton text="LOGIN"
+                                   disabled={email.length==0 ||password.length<8}
+                                   onClick={() => {
+                        loginRequest().then(value => {
+                            if (value.authorized) {
+                                props.dispatch(AuthLoginAction(value));
+                            }
+                        })
+
+                    }} allowDisabledFocus
+                    />
+                </>
+            }
         </div>
     );
 }
